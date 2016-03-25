5371e0918d7755562b4b418fa75a75b5dfcdc1c0
Bowen Masco <bowen@codingfoo.com>
2016-03-25 00:11:45 -0600

    Add initial login

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Blog' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login - Blog' });
+});
+
+router.post('/login', function(req, res, next) {
+  var sha1sum = crypto.createHash('sha1');
+
+  req.db.driver.execQuery(
+    'SELECT * FROM users WHERE email=?;',
+    [req.body.email],
+    function(err, data){
+      if(err)
+      {
+        console.log(err);
+      }
+
+      sha1sum.update(req.body.password);
+      var hashed_input = sha1sum.digest('hex');
+
+      if(hashed_input === data.password)
+      {
+        res.redirect('/entries/index');
+      }
+      else
+      {
+        res.redirect('/login');
+      }
+
+      res.render('entries/entry', {title: "a entry", entry: data[0]});
+    }
+  );
+
 });
 
 module.exports = router;
